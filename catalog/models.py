7818bb7fd256e76bad7db1f5f9d9253ccd2f4f2d
Sergey <sergey199433@gmail.com>
2024-10-02 00:05:44 +0400

    fix models.py

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Наименование продукта", help_text="Введите наименование продукта")
     description = models.CharField(max_length=150, verbose_name="Описание продукта", help_text="Опишите продукт")
-    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Изображение", help_text="Вставьте изображение продукта")
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name="Изображение", help_text="Вставьте изображение продукта")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name="Категория продукта", help_text="Введите категорию продукта", related_name="products")
     price = models.IntegerField(verbose_name="Цена продукта", help_text="Введите цену продукта")
     created_at = models.DateField(verbose_name="Дата создания продукта в БД",help_text="Введите дату создания продукта")
