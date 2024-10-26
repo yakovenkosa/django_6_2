7cb234a7a586cdd7d456ec68cb700e311b8a4fae
Sergey <sergey199433@gmail.com>
2024-10-26 12:41:24 +0400

    update file models.py.py for catalog

--- catalog/models.py
+++ catalog/models.py
@@ -39,13 +39,3 @@ class Product(models.Model):
         permissions = [
             ("can_unpublish_product", "Can unpublish product")
         ]
-
-
-class StyleFormMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for fild_name, fild in self.fields.items():
-            if isinstance(fild, BooleanField):
-                fild.widget.attrs["class"] = "form-check-input"
-            else:
-                fild.widget.attrs["class"] = "form-control"
